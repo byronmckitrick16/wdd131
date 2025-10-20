9faaaf7ae98a9eef7248b9565547a1750c686cd3
byronmckitrick16 <mck24036@byui.edu>
2025-10-20 09:58:09 -0600

    update more

---
+++ typing_test/typing_test.js
@@ -0,0 +1,5 @@
+// quick reminder for myself later
+// localStorage.setItem("nameforkey", variable)
+// localStorage.getItem("nameofkey")
+// localStorage.setItem("name", JSON.stringify(object));
+// JSON.parse(localStorage.getItem(stringToTurnToObject));
